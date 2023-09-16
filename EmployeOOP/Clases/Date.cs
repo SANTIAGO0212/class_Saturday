a6fa6a23ddb004f5c846d5693ac349e89e947698
Santiago Jimenez <santiagocj0212@gmail.com>
2023-09-16 14:52:51 -0500

    Date class created

---
+++ EmployeOOP/Clases/Date.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeOOP.Clases
+{
+    public class Date
+    {
+        #region Fields
+
+        private int _year;
+        private int _month;
+        private int _day;
+
+        #endregion
+
+        #region Methods
+
+        public Date(int day, int month, int year)
+        {
+            _year = year;
+            _month = month;
+            _day = day;
+        }
+
+        public override string ToString()
+        {
+            var dateConcatenated = _day + "/" + _month + "/" + _year;
+            return dateConcatenated;
+
+
+        }
+
+
+        #endregion
+
+
+
+
+    }
+}
--- EmployeOOP/Program.cs
+++ EmployeOOP/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeOOP.Clases;
+
+Console.WriteLine("OOP APLICATION");
+Console.WriteLine("--------------");
+
+//Inicialización
+Date dateObject = new Date(4,5,566214);
+Console.WriteLine(dateObject.ToString());
